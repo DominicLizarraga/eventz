00d76e4342bf014f68b3384a1db7690661732a90
dominic lizarraga <dominiclizarraga@hotmail.com>
2022-08-14 15:33:36 -0700

    new a create action

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,6 +2,17 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.save
+    redirect_to @event
+  end
+
   def show
     # fail
     @event = Event.find(params[:id])
@@ -13,10 +24,15 @@ class EventsController < ApplicationController
 
   def update
     @event = Event.find(params[:id])
-    event_params = params.require(:event).permit(:name, :description, :location, :price, :starts_at)
     @event.update(event_params)
     redirect_to @event
   end
 
+  private
+
+  def event_params
+    params.require(:event).
+      permit(:name, :description, :location, :price, :starts_at)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "events#index"
-  get "events", to: "events#index"
-  get "events/:id", to: "events#show", as: "event"
-  get "events/:id/edit", to: "events#edit", as: "edit_event"
-  patch "events/:id", to: "events#update"
-
+  # get "events", to: "events#index"
+  # get "events/new", to: "events#new"
+  # get "events/:id", to: "events#show", as: "event"
+  # get "events/:id/edit", to: "events#edit", as: "edit_event"
+  # patch "events/:id", to: "events#update"
+  resources :events
 
 end
