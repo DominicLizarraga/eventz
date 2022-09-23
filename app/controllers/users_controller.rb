794f7607b214edb467114a8e7248417558f5de1c
dominic lizarraga <dominiclizarraga@hotmail.com>
2022-09-22 20:38:34 -0700

    added admin modification

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
 
   before_action :require_signin, except: [:new, :create]
-  before_action :require_correct_user, only: [:edit, :update, :destroy]
-
+  before_action :require_correct_user, only: [:edit, :update]
+  before_action :require_admin, only: [:destroy]
   def index
     @users = User.all
   end
@@ -39,10 +39,10 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    # @user = User.find(params[:id])
+    @user = User.find(params[:id])
     @user.destroy
-    session[:user_id] = nil
-    redirect_to events_url, alert: "Account succesfully delted"
+    # session[:user_id] = nil
+    redirect_to root_url, alert: "Account succesfully delted"
   end
 
 
